690b87a07b96542ae65116610637570cdb4e2a1f
suramya98 <suramyas1998@gmail.com>
2024-03-29 12:46:11 +0000

    Adding New Beans to Spring Context

---
+++ example1/src/main/java/com/example/main/Example1.java
@@ -0,0 +1,27 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example1 {
+
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("Honda City");
+        System.out.println("Vehicle name from non-spring context is: " + vehicle.getName());
+
+        //Line responsible for initializing Spring IOC container
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        //Gives Spring Bean that is being maintained by the Spring IOC Container
+        Vehicle veh = context.getBean(Vehicle.class);
+
+        System.out.println("Vehicle name from Spring Context is: " + veh.getName());
+        String hello = context.getBean(String.class);
+        System.out.println("String value from Spring Context is: " + hello);
+        Integer num = context.getBean(Integer.class);
+        System.out.println("Integer value from Spring Context is: " + num);
+
+    }
+}
