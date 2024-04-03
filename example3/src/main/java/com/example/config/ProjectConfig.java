b8e657777ca64dd262380476f65f081b48d0d4bb
suramya98 <suramyas1998@gmail.com>
2024-04-03 15:11:10 +0100

    Different ways to name a bean

---
+++ example3/src/main/java/com/example/config/ProjectConfig.java
@@ -0,0 +1,28 @@
+package com.example.config;
+
+import com.example.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class ProjectConfig {
+    @Bean(name="audiVehicle")
+    Vehicle vehicle1(){
+        var veh = new Vehicle();
+        veh.setName("Audi 8");
+        return veh;
+    }
+
+    @Bean(value="hondaVehicle")
+    Vehicle vehicle2(){
+        var veh = new Vehicle();
+        veh.setName("Honda");
+        return veh;
+    }
+
+    @Bean("ferrariVehicle")
+    Vehicle vehicle3(){
+        var veh = new Vehicle();
+        veh.setName("Ferrari");
+        return veh;
+    }
+}
---
+++ example3/src/main/java/com/example/main/Example3.java
@@ -0,0 +1,24 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example3 {
+
+    public static void main(String[] args) {
+
+        //Line responsible for initializing Spring IOC container
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        //Gives Spring Bean that is being maintained by the Spring IOC Container
+        Vehicle veh1 = context.getBean("audiVehicle",Vehicle.class);
+        System.out.println("Vehicle name from Spring Context is: " + veh1.getName());
+
+        Vehicle veh2 = context.getBean("hondaVehicle",Vehicle.class);
+        System.out.println("Vehicle name from Spring Context is: " + veh2.getName());
+
+        Vehicle veh3 = context.getBean("ferrariVehicle",Vehicle.class);
+        System.out.println("Vehicle name from Spring Context is: " + veh3.getName());
+    }
+}
