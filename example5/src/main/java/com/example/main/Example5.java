89c5cf36aeb3aab37e6233b9cf2de2cac08a5c48
suramya98 <suramyas1998@gmail.com>
2024-04-04 11:55:40 +0100

    Creating bean using Component Annotation

---
+++ example5/src/main/java/com/example/main/Example5.java
@@ -0,0 +1,19 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example5 {
+
+    public static void main(String[] args) {
+
+        //Line responsible for initializing Spring IOC container
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        //Gives Spring Bean that is being maintained by the Spring IOC Container
+        Vehicle veh = context.getBean(Vehicle.class);
+        System.out.println("Component Vehicle name from Spring Context is: " + veh.getName());
+        veh.printHello();
+    }
+}
