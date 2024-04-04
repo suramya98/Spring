47eeedbca74dd9757a2b7ee8cba33c6d695d6e5e
suramya98 <suramyas1998@gmail.com>
2024-04-04 11:05:17 +0100

    Primary Annotation

---
+++ example4/src/main/java/com/example/config/ProjectConfig.java
@@ -0,0 +1,32 @@
+package com.example.config;
+
+import com.example.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ProjectConfig {
+
+    @Primary
+    @Bean(name="audiVehicle")
+    Vehicle vehicle1(){
+        var veh = new Vehicle();
+        veh.setName("Audi 8");
+        return veh;
+    }
+
+    @Bean(value="hondaVehicle")
+    Vehicle vehicle2(){
+        var veh = new Vehicle();
+        veh.setName("Honda");
+        return veh;
+    }
+
+    @Bean("ferrariVehicle")
+    Vehicle vehicle3(){
+        var veh = new Vehicle();
+        veh.setName("Ferrari");
+        return veh;
+    }
+}
