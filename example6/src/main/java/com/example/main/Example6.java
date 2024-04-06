c38cca778cbfcf4e3c46a07f0fa40a0030c24378
suramya98 <suramyas1998@gmail.com>
2024-04-06 12:44:27 +0100

    PostConstruct & PreDestroy Annotations

---
+++ example6/src/main/java/com/example/main/Example6.java
@@ -0,0 +1,21 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example6 {
+
+    public static void main(String[] args) {
+
+        //Line responsible for initializing Spring IOC container
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        //Gives Spring Bean that is being maintained by the Spring IOC Container
+        Vehicle veh = context.getBean(Vehicle.class);
+        System.out.println("Vehicle name from Spring Context is: " + veh.getName());
+        veh.printHello();
+        context.close();
+
+    }
+}
