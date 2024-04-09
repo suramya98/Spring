6b8acdcb65026bdc57c545dfe8cdc7f9a4d63c00
suramya98 <suramyas1998@gmail.com>
2024-04-09 13:03:05 +0100

    Injecting beans using Autowired annotation on setter method

---
+++ example12/src/main/java/com/example/main/Example12.java
@@ -0,0 +1,21 @@
+package com.example.main;
+
+import com.example.beans.Person;
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class Example12 {
+
+    public static void main(String[] args) {
+
+        //Line responsible for initializing Spring IOC container
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Person person = context.getBean(Person.class);
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println("Person name from Spring Context is:" + person.getName());
+        System.out.println("Vehicle name from Spring Context is:" + vehicle.getName());
+        System.out.println("Vehicle that person own is:" + person.getVehicle());
+    }
+}
