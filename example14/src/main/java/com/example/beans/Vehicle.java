f6f4b4ab1d3ed4d71f692e8ab7a4e96ca7402542
suramya98 <suramyas1998@gmail.com>
2024-04-10 11:54:17 +0100

    Types of Autowiring and Qualifier Annotation

---
+++ example14/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,26 @@
+package com.example.beans;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Vehicle {
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void printHello(){
+        System.out.println("Printing Hello from Component Vehicle Bean");
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ example14/src/main/java/com/example/config/ProjectConfig.java
@@ -0,0 +1,34 @@
+package com.example.config;
+
+import com.example.beans.Person;
+import com.example.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@ComponentScan(basePackages = "com.example.beans")
+public class ProjectConfig {
+    @Bean
+    //@Primary
+    Vehicle vehicle1() {
+        var veh = new Vehicle();
+        veh.setName("Audi");
+        return veh;
+    }
+
+    @Bean
+    Vehicle vehicle2() {
+        var veh = new Vehicle();
+        veh.setName("Honda");
+        return veh;
+    }
+
+    @Bean
+    Vehicle vehicle3() {
+        var veh = new Vehicle();
+        veh.setName("Ferrari");
+        return veh;
+    }
+}
